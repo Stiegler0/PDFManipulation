709f5e2c7b79e633c73be5345846fbcd83782389
yassine <yassine.jemlaoui@epita.fr>
2023-10-06 15:24:30 +0200

    lock fixed

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import argparse
+from pypdf import PdfReader, PdfWriter
 
 def argument_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('file',help='pdf file name')
     parser.add_argument('-r','--read',action='store_true',help='read and print the file')
     parser.add_argument('-m','--filedata',action='store_true')
+    parser.add_argument('-l','--lock',action='store_true')
     args = parser.parse_args()
     #access to the args
     read_mode = args.read
     file_name = args.file
     file_data = args.filedata
-    return file_name,read_mode,file_data 
+    file_lock = args.lock
+    return file_name,read_mode,file_data, file_lock
 
 
 def read_file(file_name):
@@ -19,19 +22,23 @@ def read_file(file_name):
     return(content)#.replace("\n",""))
 
 def metadata(file_name):
-    from pypdf import PdfReader
     reader = PdfReader(file_name)
     meta = reader.metadata
-    #with open(file_name, 'rb') as f:
-     #   content = f.read()
     author = meta.author
     title = meta.title
     subject = meta.subject
     return author,title,subject
 
-
+def lock(file_name,pwd):
+        reader = PdfReader(file_name)
+        writer = PdfWriter()
+        for page in reader.pages:
+            writer.add_page(page)
+        writer.encrypt(pwd)
+        with open(file_name,"wb") as f:
+            writer.write(f)
 if __name__ == "__main__":
-    file_name, read_mode,file_data= argument_parser()
+    file_name, read_mode,file_data, file_lock= argument_parser()
     #print(file_name)
     #print(read_mode)
 
@@ -41,4 +48,9 @@ if __name__ == "__main__":
 
     elif file_data:
         print(metadata(file_name))
+
+    elif file_lock:
+        password = input('saisie un mot de passe: ')
+        (lock(file_name,password))
+
 
